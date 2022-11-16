85417550c2c773ec2fe9e9d9d9a2dbc69f44db36
Martin-Moreno-Jara <martin.moreno.j04@gmail.com>
2022-11-16 17:26:06 -0500

    se quita look and feel

--- src/Logic/Main.java
+++ src/Logic/Main.java
@@ -1,19 +1,6 @@
 package Logic;
 
 import UI.*;
-import com.jtattoo.plaf.AbstractLookAndFeel;
-import com.jtattoo.plaf.acryl.AcrylLookAndFeel;
-import com.jtattoo.plaf.aero.AeroLookAndFeel;
-import com.jtattoo.plaf.bernstein.BernsteinLookAndFeel;
-import com.jtattoo.plaf.fast.FastLookAndFeel;
-import com.jtattoo.plaf.graphite.GraphiteLookAndFeel;
-import com.jtattoo.plaf.hifi.HiFiLookAndFeel;
-import com.jtattoo.plaf.luna.LunaLookAndFeel;
-import com.jtattoo.plaf.mcwin.McWinLookAndFeel;
-import com.jtattoo.plaf.mint.MintLookAndFeel;
-import com.jtattoo.plaf.noire.NoireLookAndFeel;
-import com.jtattoo.plaf.smart.SmartLookAndFeel;
-import com.jtattoo.plaf.texture.TextureLookAndFeel;
 
 import javax.swing.*;
 import javax.swing.plaf.metal.MetalLookAndFeel;
@@ -21,16 +8,17 @@ import javax.swing.plaf.multi.MultiLookAndFeel;
 import javax.swing.plaf.nimbus.NimbusLookAndFeel;
 import javax.swing.plaf.synth.SynthLookAndFeel;
 
-public class Main extends Instancias{
+public class Main extends Instancias {
 
-    public static void main(String [] args) {
+    public static void main(String[] args) {
         try {
-            UIManager.setLookAndFeel(new TextureLookAndFeel() {
-            });
+            UIManager.setLookAndFeel(new NimbusLookAndFeel());
         } catch (UnsupportedLookAndFeelException e) {
             e.printStackTrace();
         }
+
         GUI gui = new GUI();
         gui.setVisible(true);
-            }
+
+    }
 }
